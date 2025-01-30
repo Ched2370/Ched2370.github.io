f11d2bf222e112828d8fe19022b0d01a19e4c2de
Ched2370 <mmibz.85@gmail.com>
2025-01-30 18:30:31 -0300

    Correccion saque animejs
    para mejor funsionamiento saque la libreria animejs

--- porfolio/script.js
+++ porfolio/script.js
@@ -38,3 +38,30 @@ function seleccionar() {
 /* window.onscroll = function () {
   efectoHabilidades();
 }; */
+
+const container = document.querySelector('.container');
+for (let i = 0; i < 100; i++) {
+  const blocks = document.createElement('div');
+  blocks.classList.add('block');
+  container.appendChild(blocks);
+}
+
+function animeteBlocks() {
+  anime({
+    targets: '.block',
+    translateX: function () {
+      return anime.random(-700, 700);
+    },
+    translateY: function () {
+      return anime.random(-500, 500);
+    },
+    scale: function () {
+      return anime.random(1, 5);
+    },
+    easing: 'linear',
+    duration: 4000,
+    delay: anime.stagger(10),
+    loopComplete: animeteBlocks,
+  });
+}
+animeteBlocks();
